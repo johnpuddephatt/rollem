9e4c68357a631bada92fa2ee48f98cacbf723421
John Puddephatt <john@jdp.org.uk>
2023-03-02 16:07:38 +0000

    posts page fix

--- resources/views/flexible/featured-post.blade.php
+++ resources/views/flexible/featured-post.blade.php
@@ -1,12 +1,14 @@
-<a href="{{ $layout->post->url }}" class="relative lg:grid lg:grid-cols-2">
-    @if ($layout->post->image)
-        <x-image conversion="3x2" class="mt-16 block w-full" :image="$layout->post->image" />
-        <div class="absolute top-0 right-1/2 hidden h-16 w-16 bg-teal md:block"></div>
-    @endif
-    <div class="relative z-10 flex flex-col justify-center bg-teal p-12 text-white max-md:ml-8 max-md:-mt-8">
+@if ($layout->post)
+    <a href="{{ $layout->post->url }}" class="relative lg:grid lg:grid-cols-2">
+        @if ($layout->post->image)
+            <x-image conversion="3x2" class="mt-16 block w-full" :image="$layout->post->image" />
+            <div class="absolute top-0 right-1/2 hidden h-16 w-16 bg-teal md:block"></div>
+        @endif
+        <div class="relative z-10 flex flex-col justify-center bg-teal p-12 text-white max-md:ml-8 max-md:-mt-8">
 
-        <h1 class="mb-6 text-5xl font-bold">{!! $layout->post->title !!}</h1>
-        <div class="mb-8 max-w-md">{!! $layout->post->introduction !!}</div>
-        <p class="w-36 border-b-2 border-lilac pb-2 text-left text-sm font-semibold">Read more</p>
-    </div>
-</a>
+            <h1 class="mb-6 text-5xl font-bold">{!! $layout->post->title !!}</h1>
+            <div class="mb-8 max-w-md">{!! $layout->post->introduction !!}</div>
+            <p class="w-36 border-b-2 border-lilac pb-2 text-left text-sm font-semibold">Read more</p>
+        </div>
+    </a>
+@endif
--- resources/views/flexible/latest-posts.blade.php
+++ resources/views/flexible/latest-posts.blade.php
@@ -1,4 +1,4 @@
-@if (isset($layout->posts))
+@if ($layout->posts)
     <div class="container my-16 grid gap-16 lg:grid-cols-3">
         @foreach ($layout->posts as $post)
             <x-post-card class="" :post="$post" />
