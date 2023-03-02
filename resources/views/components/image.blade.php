0c48d250358b81977bceba3f0bfdce62795b338e
John Puddephatt <john@jdp.org.uk>
2023-03-02 11:56:52 +0000

    remove dump in image

--- resources/views/components/image.blade.php
+++ resources/views/components/image.blade.php
@@ -1,5 +1,3 @@
-@dump($attributes)
-
 <img {{ $attributes }} src="{{ $getSrc($conversion) }}" srcset="{{ $getSrcSet() }}"
     onload="window.requestAnimationFrame(function(){if(!(size=getBoundingClientRect().width))return;onload=null;sizes=Math.ceil(size/window.innerWidth*100)+'vw';});"
     sizes="1px" />
