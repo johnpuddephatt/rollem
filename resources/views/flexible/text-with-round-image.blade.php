063cb05cf911f3d7ff13677c0db864a0fe6c4a74
John Puddephatt <john@jdp.org.uk>
2023-03-02 10:57:32 +0000

    change to thumbnail image on text w/round image

--- resources/views/flexible/text-with-round-image.blade.php
+++ resources/views/flexible/text-with-round-image.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-white py-12">
     <div class="container flex-row items-center justify-between gap-4 lg:flex lg:gap-8">
-        <x-image :image="$layout->image" conversion="3x2"
+        <x-image :image="$layout->image" conversion="thumbnail"
             class="{{ $layout->large ? 'h-64 w-64 lg:h-96 lg:w-96' : 'h-48 w-48 lg:h-64' }} rounded-full object-cover lg:w-64" />
         <div class="@if ($layout->reverse) lg:-order-1 @endif prose col-span-2 max-w-2xl pt-8 lg:py-16">
             @if ($layout->title)
