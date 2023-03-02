0c4e1d239753deb68b2dcad638937fc46d7bc422
John Puddephatt <john@jdp.org.uk>
2023-03-02 11:13:13 +0000

    fix color band on images

--- resources/views/flexible/image.blade.php
+++ resources/views/flexible/image.blade.php
@@ -1,8 +1,8 @@
 <div class="relative bg-white py-24">
     @if ($layout->background_colour)
-        <div class="bg-{{ $layout->background_colour }} absolute bottom-0 left-0 -z-10 h-48 w-full"></div>
+        <div class="bg-{{ $layout->background_colour }} absolute bottom-0 left-0 h-48 w-full"></div>
     @endif
-    <div class="container">
+    <div class="container relative">
         <x-image conversion="3x2" class="block w-full max-w-none" :image="$layout->image" />
     </div>
 </div>
