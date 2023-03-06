ef5eb705dc40aed1555d3c219ab9d6f7b1444942
John Puddephatt <john@jdp.org.uk>
2023-03-06 20:34:19 +0000

    tweak subproduction title

--- resources/views/flexible/sub-productions.blade.php
+++ resources/views/flexible/sub-productions.blade.php
@@ -7,11 +7,11 @@
                         @click="$el.scrollIntoView(); currentSubProduction = '{{ $subProduction->slug }}'">
                         @if ($subProduction->image)
                             <x-image ::class="{ '!border-lilac': currentSubProduction == '{{ $subProduction->slug }}' }"
-                                class="border-transparent block aspect-[3/2] h-auto w-full border-4 transition group-hover:border-white"
+                                class="block aspect-[3/2] h-auto w-full border-4 border-transparent transition group-hover:border-white"
                                 conversion="3x2" :image="$subProduction->image" />
                         @else
                             <div :class="{ '!border-lilac': currentSubProduction == '{{ $subProduction->slug }}' }"
-                                class="border-transparent block aspect-[3/2] h-auto w-full border-4 bg-gray bg-opacity-20 transition group-hover:border-white">
+                                class="block aspect-[3/2] h-auto w-full border-4 border-transparent bg-gray bg-opacity-20 transition group-hover:border-white">
                             </div>
                         @endif
                         <h3 class="pt-2 text-lg font-semibold">{{ $subProduction->title }}</h3>
@@ -24,7 +24,18 @@
         <div id="subproduction-{{ $subProduction->slug }}"
             x-show="currentSubProduction == '{{ $subProduction->slug }}'">
 
-            <h2 class="container mt-16 mb-24 text-6xl font-bold">{{ $subProduction->title }}</h2>
+            <div class="relative overflow-hidden">
+                <div class="grid-cols-3 lg:container lg:grid">
+                    <div class="col-span-2 py-16 max-lg:container">
+                        <h2 class="container mt-16 mb-24 text-6xl font-bold">{{ $subProduction->title }}</h2>
+
+                    </div>
+                    <div
+                        class="relative py-16 before:absolute before:left-0 before:top-0 before:bottom-0 before:-z-10 before:block before:w-[99999px] before:bg-gray lg:px-8">
+                    </div>
+                </div>
+            </div>
+
             @foreach ($subProduction->content as $layout)
                 @include('flexible.' . $layout->name(), ['layout' => $layout])
             @endforeach
