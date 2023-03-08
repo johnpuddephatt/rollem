0ac0d5a84197c287711662b90c628ff024bb1eec
John Puddephatt <john@jdp.org.uk>
2023-03-08 23:25:10 +0000

    video sticker

--- resources/views/flexible/watch-video.blade.php
+++ resources/views/flexible/watch-video.blade.php
@@ -5,7 +5,7 @@
             <x-image :image="$layout->image" conversion="3x2" class="block w-full" />
 
             <div
-                class="to-transparent {{ $layout->colour == 'red' ? 'right-0' : 'right-64' }} absolute bottom-0 left-0 flex w-full flex-row items-end gap-4 bg-gradient-to-t from-[#000000aa] px-4 pt-24 pb-4 lg:gap-8 lg:px-12 lg:pb-8">
+                class="{{ $layout->colour == 'red' ? 'right-0' : 'right-64' }} absolute bottom-0 left-0 flex w-full flex-row items-end gap-4 bg-gradient-to-t from-[#000000aa] to-transparent px-4 pt-24 pb-4 lg:gap-8 lg:px-12 lg:pb-8">
                 @svg('play', 'h-16 w-16 lg:h-24 lg:w-24')
                 <div>
                     @if ($layout->pretitle)
@@ -21,7 +21,7 @@
                 <div
                     class="{{ $layout->colour == 'red' ? 'bg-red' : 'bg-white' }} absolute right-1 top-1 flex h-36 w-36 flex-col justify-center overflow-hidden rounded-full p-4 pr-0 text-lg font-bold leading-tight lg:top-8 lg:right-16 lg:h-64 lg:w-64 lg:p-16 lg:text-3xl">
                     <div
-                        class="text-{{ $layout->colour }} {{ $layout->colour == 'red' ? 'border-white text-white' : 'last-child:text-black border-' . $layout->colour }} border-b-2 pb-1 pr-4 lg:pr-16">
+                        class="text-{{ $layout->colour }} {{ $layout->colour == 'red' ? 'border-white text-white' : 'last-child:text-black border-' . $layout->colour }} border-b-2 pb-1 pr-4 lg:pr-8">
                         {!! Str::markdown($layout->badge) !!}
                     </div>
                 </div>
