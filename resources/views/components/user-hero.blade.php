70adaf1f55dd84283d8fcbd947fec5dfa0d914b1
John Puddephatt <john@jdp.org.uk>
2023-03-08 12:47:32 +0000

    user page layout fix

--- resources/views/components/user-hero.blade.php
+++ resources/views/components/user-hero.blade.php
@@ -1,11 +1,10 @@
 <div class="relative flex h-[75vh] flex-col justify-end gap-x-16 bg-red pb-16 pt-48 text-white">
     <div class="container relative w-full pt-32">
-
-        <h1 class="wrap-words max-w-6xl text-7xl font-bold !tracking-normal lg:text-8xl">
+        <x-image conversion="2x3"
+            class="right-0 top-0 z-[20] mb-12 ml-auto h-auto w-3/4 sm:w-1/2 lg:absolute lg:mb-0 lg:w-2/5"
+            :image="$user->photo" />
+        <h1 class="lg:wrap-words max-w-6xl text-7xl font-bold !tracking-normal lg:text-8xl">
             {{ $user->name }}
         </h1>
-
-        <x-image conversion="2x3" class="absolute right-0 top-0 z-[20] h-auto w-2/5" :image="$user->photo" />
-
     </div>
 </div>
--- resources/views/flexible/quote.blade.php
+++ resources/views/flexible/quote.blade.php
@@ -2,8 +2,8 @@
     class="bg-{{ $layout->background_colour ?? 'teal' }} flex flex-col justify-center py-32 text-center lg:py-48 lg:text-left">
     <div class="container">
 
-        <p class="text-{{ $layout->quote_colour ?? 'white' }} max-w-7xl text-5xl font-bold italic lg:text-8xl"
-            style="text-indent: -1ch">
+        <p
+            class="text-{{ $layout->quote_colour ?? 'white' }} quote-indent max-w-7xl text-5xl font-bold italic lg:text-8xl 2xl:-indent-10">
             “ <span class="text-{{ $layout->text_colour ?? 'lilac' }}">{{ $layout->quote }}</span>&nbsp;”
         </p>
         @if ($layout->source)
