49e7d47c01e3ee448b1b61caa30c73e8a15dbe66
John Puddephatt <john@jdp.org.uk>
2023-03-08 00:39:58 +0000

    stop post title clashing with logo

--- resources/views/flexible/post-hero.blade.php
+++ resources/views/flexible/post-hero.blade.php
@@ -1,4 +1,4 @@
-<div class="relative -z-10 flex h-screen flex-col justify-end bg-black pb-64 text-white lg:h-[75vh]">
+<div class="relative -z-10 flex h-screen flex-col justify-end bg-black pb-32 text-white lg:h-[80vh]">
     <div class="pointer-events-none absolute top-0 left-0 right-0 h-64 bg-gradient-to-b from-[#000000aa] to-transparent">
     </div>
     <div id="hero-text" class="container">
@@ -7,6 +7,6 @@
     </div>
 </div>
 
-<div class="ml-auto -mt-48 lg:w-3/4 xl:w-2/3 2xl:w-1/2">
+<div class="ml-auto -mt-24 lg:w-3/4 xl:w-2/3 2xl:w-1/2">
     <x-image conversion="3x2" :image="$layout->image" class="h-full w-full object-cover" />
 </div>
