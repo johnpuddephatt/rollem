a740daac6f98d5385b28b41b8659211bbd59e306
John Puddephatt <john@jdp.org.uk>
2023-03-09 00:06:13 +0000

    awards box

--- resources/views/flexible/text-with-image.blade.php
+++ resources/views/flexible/text-with-image.blade.php
@@ -1,10 +1,11 @@
-    <div class="relative py-36">
+    <div class="relative py-24">
 
-        <div class="left-0 top-32 -z-10 min-h-[65%] w-full bg-gray py-8 lg:py-36">
-            <div class="{{ $layout->reverse ? '' : 'justify-between' }} container flex flex-col gap-24 lg:flex-row">
+        <div class="left-0 top-32 -z-10 min-h-[65%] w-full py-8 lg:py-36">
+            <div
+                class="{{ $layout->reverse ? '' : 'justify-between' }} container flex flex-col items-center gap-24 lg:flex-row">
                 <div class="{{ $layout->reverse ? 'order-last' : '' }} prose max-w-lg">
                     @if ($layout->title)
-                        <h2 class="mt-0 mb-0 text-4xl font-bold">{{ $layout->title }}</h2>
+                        <h2 class="mt-0 mb-0 text-4xl font-bold lg:text-6xl">{{ $layout->title }}</h2>
                     @endif
                     @if ($layout->subtitle)
                         <h3 class="mt-2 mb-0 text-xl font-bold">{{ $layout->subtitle }}</h3>
@@ -14,13 +15,13 @@
                 </div>
 
                 <x-image :image="$layout->image" conversion="square"
-                    class="{{ $layout->reverse ? 'order-first' : '' }} -my-16 block max-w-none object-cover lg:-my-64 lg:w-1/2" />
+                    class="{{ $layout->reverse ? 'order-first' : '' }} block max-w-none object-cover lg:w-1/2" />
             </div>
         </div>
 
         @if ($layout->badge)
             <div
-                class="absolute right-1 top-1 top-8 flex h-36 w-36 flex-col justify-center overflow-hidden rounded-full bg-red p-4 pr-0 text-lg font-bold leading-tight lg:left-1/2 lg:h-64 lg:w-64 lg:-translate-x-1/2 lg:p-16 lg:pr-0 lg:text-3xl">
+                class="absolute right-1 top-20 flex h-36 w-36 flex-col justify-center overflow-hidden rounded-full bg-red p-4 pr-0 text-lg font-bold leading-tight lg:left-1/2 lg:h-64 lg:w-64 lg:-translate-x-1/2 lg:p-16 lg:pr-0 lg:text-3xl">
                 <div class="border-b-2 border-white pb-1 pr-4 text-white lg:pr-16">
                     {!! Str::markdown($layout->badge) !!}
                 </div>
