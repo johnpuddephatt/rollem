3b7c153246805b3cb492830d836d83dc2221875d
John Puddephatt <john@jdp.org.uk>
2023-03-09 23:44:49 +0000

    tweak

--- resources/views/flexible/text-with-sidebar.blade.php
+++ resources/views/flexible/text-with-sidebar.blade.php
@@ -6,7 +6,7 @@
             </div>
         </div>
         <div
-            class="{{ $layout->reverse ? 'order-first' : '' }} relative before:absolute before:left-0 before:top-0 before:bottom-0 before:-z-10 before:block before:w-[99999px] before:bg-gray lg:px-8 lg:py-16">
+            class="{{ $layout->reverse ? 'order-first' : '' }} relative pt-12 before:absolute before:left-0 before:top-0 before:bottom-0 before:-z-10 before:block before:w-[99999px] before:bg-gray lg:px-8 lg:py-16">
             <div class="{{ $layout->reverse ? '' : 'ml-auto' }} max-w-xs max-lg:container">
                 <h3 class="mb-6 text-lg font-semibold">{{ $layout->sidebar_title }}</h3>
                 <div class="prose">
