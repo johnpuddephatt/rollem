8ddd9a2a18164da771d543ef49a8d3b701081163
John Puddephatt <john@jdp.org.uk>
2023-03-09 23:45:36 +0000

    tweaks

--- resources/views/flexible/text-with-pullout.blade.php
+++ resources/views/flexible/text-with-pullout.blade.php
@@ -6,7 +6,7 @@
             </div>
         </div>
         @if ($layout->sidebar)
-            <div class="{{ $layout->reverse ? 'order-first' : 'order-first lg:order-none' }} pb-8 lg:py-16">
+            <div class="{{ $layout->reverse ? 'order-first' : 'order-first lg:order-none' }} py-8 lg:py-16">
 
                 <div
                     class="{{ $layout->reverse ? '' : 'lg:ml-auto' }} prose max-w-xl text-xl font-bold italic lg:max-w-xs">
