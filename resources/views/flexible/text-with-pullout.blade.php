618e19dc9d7a02e639a67e1b7420aeff2f9cb0bf
John Puddephatt <john@jdp.org.uk>
2023-03-09 00:16:42 +0000

    build

--- resources/views/flexible/text-with-pullout.blade.php
+++ resources/views/flexible/text-with-pullout.blade.php
@@ -9,7 +9,7 @@
             <div class="{{ $layout->reverse ? 'order-first' : '' }} py-16">
 
                 <div
-                    class="{{ $layout->reverse ? '' : 'ml-auto' }} prose max-w-xl text-xl font-semibold italic lg:max-w-xs">
+                    class="{{ $layout->reverse ? '' : 'lg:ml-auto' }} prose max-w-xl text-xl font-semibold italic lg:max-w-xs">
                     <hr class="mb-4 w-48 max-w-full border-t-2 border-teal-light" />
                     {!! $layout->sidebar !!}
                 </div>
