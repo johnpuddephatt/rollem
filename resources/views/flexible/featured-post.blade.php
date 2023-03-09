5f5e0166512a4ecfa300786012211701a7eac733
John Puddephatt <john@jdp.org.uk>
2023-03-09 23:27:22 +0000

    posts page

--- resources/views/flexible/featured-post.blade.php
+++ resources/views/flexible/featured-post.blade.php
@@ -1,8 +1,8 @@
 @if ($layout->post)
-    <a href="{{ $layout->post->url }}" class="relative lg:grid lg:grid-cols-2">
+    <a href="{{ $layout->post->url }}" class="relative bg-white pt-16 lg:grid lg:grid-cols-2">
         @if ($layout->post->image)
-            <x-image conversion="3x2" class="mt-16 block w-full" :image="$layout->post->image" />
-            <div class="absolute top-0 right-1/2 hidden h-16 w-16 bg-teal md:block"></div>
+            <x-image conversion="3x2" class="block w-full" :image="$layout->post->image" />
+            <div class="absolute top-0 right-1/2 hidden h-16 w-16 bg-teal lg:block"></div>
         @endif
         <div class="relative z-10 flex flex-col justify-center bg-teal p-12 text-white max-md:ml-8 max-md:-mt-8">
 
--- resources/views/flexible/featured-posts.blade.php
+++ resources/views/flexible/featured-posts.blade.php
@@ -1,12 +1,14 @@
 @if ($layout->posts->count())
-    <div class="bg-white pb-8 pt-0 max-lg:container lg:pb-24 lg:pt-0">
-        <x-post-card class="block md:grid md:grid-cols-2" :wide="true" :post="$layout->posts->first()" />
-        <div class="lg:container">
-            <div class="gap-16 md:mt-24 md:grid md:grid-cols-3">
-                <div class="border-teal-light xl:border-r-2"></div>
-                @foreach ($layout->posts->skip(1) as $post)
-                    <x-post-card class="flex flex-col" :post="$post" />
-                @endforeach
+    <div class="bg-white pb-8 pt-0 lg:pb-24 lg:pt-0">
+        <div class="max-md:container">
+            <x-post-card class="block md:grid md:grid-cols-2" :wide="true" :post="$layout->posts->first()" />
+            <div class="lg:container">
+                <div class="gap-16 md:mt-24 md:grid md:grid-cols-3">
+                    <div class="border-teal-light xl:border-r-2"></div>
+                    @foreach ($layout->posts->skip(1) as $post)
+                        <x-post-card class="flex flex-col" :post="$post" />
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
