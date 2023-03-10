888c132f0603a9f13afa99079f3436f797bdfea2
John Puddephatt <john@jdp.org.uk>
2023-03-10 10:28:21 +0000

    user profiles

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -4,10 +4,10 @@
 @extends('layouts.default') @section('content')
     @include('components.user-hero')
 
-    <div class="min-h-[50vh] bg-white pt-16 pb-32">
+    <div class="min-h-[50vh] bg-white pt-8 pb-32 lg:pt-16">
         <div class="container">
             <div class="text-3xl font-bold lg:text-5xl">{{ $user->role }}</div>
-            <div class="prose mt-16">
+            <div class="lg prose mt-8 lg:mt-16">
                 {!! $user->biography !!}
             </div>
         </div>
