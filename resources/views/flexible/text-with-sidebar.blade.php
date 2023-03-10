97cbda5f2fb0d52aaee6072eb5fa29c426ebc0c5
John Puddephatt <john@jdp.org.uk>
2023-03-10 10:39:47 +0000

    fix sidebar

--- resources/views/flexible/text-with-sidebar.blade.php
+++ resources/views/flexible/text-with-sidebar.blade.php
@@ -7,7 +7,7 @@
         </div>
         <div
             class="{{ $layout->reverse ? 'order-first' : '' }} relative py-12 before:absolute before:left-0 before:top-0 before:bottom-0 before:-z-10 before:block before:w-[99999px] before:bg-gray lg:px-8 lg:py-16">
-            <div class="{{ $layout->reverse ? '' : 'ml-auto' }} max-w-xs max-lg:container">
+            <div class="{{ $layout->reverse ? '' : 'ml-auto' }} max-lg:container lg:max-w-xs">
                 <h3 class="mb-6 text-lg font-semibold">{{ $layout->sidebar_title }}</h3>
                 <div class="prose">
                     {!! $layout->sidebar !!}
