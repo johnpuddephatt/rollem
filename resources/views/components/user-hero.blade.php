1db25b5d1e2ed4df783d2648878c653faeb60432
John Puddephatt <john@jdp.org.uk>
2023-03-10 10:30:32 +0000

    user profiles

--- resources/views/components/user-hero.blade.php
+++ resources/views/components/user-hero.blade.php
@@ -1,8 +1,9 @@
 <div class="relative flex h-[calc(100vh-2rem)] flex-col justify-end gap-x-16 bg-red pb-16 pt-48 text-white lg:h-[75vh]">
-    <div class="container relative w-full pt-32">
-        <x-image conversion="2x3" class="right-0 top-0 z-[20] mb-12 ml-auto h-auto w-1/2 lg:absolute lg:mb-0 lg:w-2/5"
+    <div class="container relative w-full pt-16 lg:pt-32">
+        <x-image conversion="2x3"
+            class="right-0 top-0 z-[20] mb-8 ml-auto h-auto w-1/2 lg:absolute lg:mb-12 lg:mb-0 lg:w-2/5"
             :image="$user->photo" />
-        <h1 class="wrap-words max-w-6xl text-5xl font-bold !tracking-normal lg:w-3/5 lg:text-8xl">
+        <h1 class="wrap-words max-w-6xl text-6xl font-bold !tracking-normal lg:w-3/5 lg:text-8xl">
             {{ $user->name }}
         </h1>
     </div>
