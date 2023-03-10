b853ad178bbc4a8db94eeee5db87deafa13c59fb
John Puddephatt <john@jdp.org.uk>
2023-03-10 00:05:36 +0000

    count posts

--- resources/views/flexible/latest-posts.blade.php
+++ resources/views/flexible/latest-posts.blade.php
@@ -1,4 +1,4 @@
-@if ($layout->posts)
+@if ($layout->posts->count())
     <div class="container my-16 grid gap-16 bg-white lg:grid-cols-3">
         @foreach ($layout->posts as $post)
             <x-post-card class="" :post="$post" />
