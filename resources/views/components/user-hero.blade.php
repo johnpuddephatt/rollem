aa8e825d0c0e308d2b580255e9603d7c87627d17
John Puddephatt <john@jdp.org.uk>
2023-03-10 16:50:28 +0000

    user page overlap

--- resources/views/components/user-hero.blade.php
+++ resources/views/components/user-hero.blade.php
@@ -1,6 +1,6 @@
 <div
     class="relative flex h-[calc(100vh-2rem)] flex-col justify-end gap-x-16 bg-red pb-8 pt-48 text-white lg:h-[75vh] lg:pb-16">
-    <div class="container relative w-full pt-16 lg:pt-32">
+    <div class="container relative w-full pt-16 2xl:pt-32">
         <x-image conversion="2x3"
             class="right-0 top-0 z-[20] mb-4 ml-auto h-auto w-2/3 sm:w-1/2 lg:absolute lg:mb-0 lg:w-2/5"
             :image="$user->photo" />
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -7,7 +7,7 @@
     <div class="min-h-[50vh] bg-white pt-8 pb-32 lg:pt-16">
         <div class="container">
             <div class="text-3xl font-bold lg:text-5xl">{{ $user->role }}</div>
-            <div class="lg prose mt-8 lg:mt-16">
+            <div class="prose mt-8 lg:mt-16">
                 {!! $user->biography !!}
             </div>
         </div>
