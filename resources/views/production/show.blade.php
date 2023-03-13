a5dc92bda3c606377f5bb8bdb668e88e13078222
John Puddephatt <john@jdp.org.uk>
2023-03-13 11:46:07 +0000

    rel prod spacing

--- resources/views/production/show.blade.php
+++ resources/views/production/show.blade.php
@@ -7,7 +7,7 @@
 
     <div class="container mt-12 lg:mt-16">
         <h2 class="mb-12 text-3xl font-bold lg:text-4xl">More of our productions</h2>
-        <div class="my-8 grid gap-8 bg-white lg:my-16 lg:grid-cols-3 lg:gap-16">
+        <div class="my-8 grid gap-12 bg-white lg:my-16 lg:grid-cols-3 lg:gap-16">
             @foreach ($related_productions as $related_production)
                 <x-production-card-small :production="$related_production" :flip="!($loop->iteration % 2)" />
             @endforeach
