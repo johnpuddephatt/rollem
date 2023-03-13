511b1e5eeca6ae9e906d99db061db20beb6a337c
John Puddephatt <john@jdp.org.uk>
2023-03-13 11:28:48 +0000

    padding

--- resources/views/flexible/text.blade.php
+++ resources/views/flexible/text.blade.php
@@ -1,4 +1,4 @@
-<div class="container py-12 lg:py-16">
+<div class="container py-8 lg:py-16">
     <div class="has-blockquotes-in-sidebar prose relative">
         {!! $layout->content !!}
     </div>
