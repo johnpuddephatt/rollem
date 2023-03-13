1b2f5101480c3f397967c99f89bc37cb6363bd0a
John Puddephatt <john@jdp.org.uk>
2023-03-13 11:03:36 +0000

    spacing

--- resources/views/flexible/text-with-pullout.blade.php
+++ resources/views/flexible/text-with-pullout.blade.php
@@ -1,12 +1,12 @@
-<div class="bg-white">
-    <div class="container grid lg:grid-cols-3">
-        <div class="col-span-2 py-8 lg:py-16">
+<div class="bg-white py-8 lg:py-16">
+    <div class="container grid gap-8 lg:grid-cols-3 lg:gap-0">
+        <div class="col-span-2">
             <div class="{{ $layout->reverse ? 'ml-auto' : '' }} prose">
                 {!! $layout->main !!}
             </div>
         </div>
         @if ($layout->sidebar !== '<p></p>')
-            <div class="{{ $layout->reverse ? 'order-first' : 'order-first lg:order-none' }} py-8 lg:py-16">
+            <div class="{{ $layout->reverse ? 'order-first' : 'order-first lg:order-none' }}">
 
                 <div class="{{ $layout->reverse ? '' : 'lg:ml-auto' }} prose text-xl font-bold italic lg:max-w-sm">
                     <hr class="mb-4 w-48 max-w-full border-t-2 border-teal-light" />
