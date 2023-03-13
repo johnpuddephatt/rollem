7aaebb532eef01e284813e6939ae0b7970b54ab4
John Puddephatt <john@jdp.org.uk>
2023-03-13 21:13:40 +0000

    page nav

--- resources/views/components/page-navigation.blade.php
+++ resources/views/components/page-navigation.blade.php
@@ -3,13 +3,11 @@
     <div class="container grid grid-cols-2">
         <h2 class="text-4xl font-bold text-teal">More in this section</h2>
         <div class="px-12">
-            <hr class="mb-4 w-48 max-w-full border-t-2 border-teal-light" />
-
-            <h3 class="mb-4 text-3xl font-bold"><a
-                    class="@if (url($page->url) == request()->url()) border-lilac @else border-transparent @endif border-b-4"
-                    href="{{ $page->url }}">{{ $page->title }}</a></h3>
-
             <nav class="space-y-2">
+                <p class="text-2xl font-bold"><a
+                        class="@if (url($page->url) == request()->url()) border-lilac @else border-transparent @endif border-b-4"
+                        href="{{ $page->url }}">{{ $page->title }}</a></p>
+
                 @foreach ($page->children as $child)
                     <p class="text-2xl font-bold"><a
                             class="@if (url($child->url) == request()->url()) border-lilac @else border-transparent @endif border-b-4"
