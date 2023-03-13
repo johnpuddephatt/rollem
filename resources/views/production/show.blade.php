fcf66003827f422cd7be08089a0f097baa67a310
John Puddephatt <john@jdp.org.uk>
2023-03-13 11:40:59 +0000

    related productions

--- resources/views/production/show.blade.php
+++ resources/views/production/show.blade.php
@@ -5,9 +5,9 @@
         @include('flexible.' . $layout->name(), ['layout' => $layout])
     @endforeach
 
-    <div class="container mt-16">
-        <h2 class="mb-12 text-4xl font-bold">More of our productions</h2>
-        <div class="my-16 grid gap-16 bg-white lg:grid-cols-3">
+    <div class="container mt-8 lg:mt-16">
+        <h2 class="mb-12 text-3xl font-bold lg:text-4xl">More of our productions</h2>
+        <div class="my-8 grid gap-8 bg-white lg:my-16 lg:grid-cols-3 lg:gap-16">
             @foreach ($related_productions as $related_production)
                 <x-production-card-small :production="$related_production" :flip="!($loop->iteration % 2)" />
             @endforeach
