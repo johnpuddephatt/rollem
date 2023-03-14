7faa18f857a00de9db5f70b72f194dd87e014f12
John Puddephatt <john@jdp.org.uk>
2023-03-14 09:52:57 +0000

    fix our team link

--- resources/views/components/user-hero.blade.php
+++ resources/views/components/user-hero.blade.php
@@ -5,7 +5,7 @@
             class="right-0 top-0 z-[20] mb-4 ml-auto h-auto w-2/3 sm:w-1/2 lg:absolute lg:-mt-24 lg:mb-0 lg:w-2/5"
             :image="$user->photo" />
         <a class="mb-2 inline-block border-b-2 border-[#fff3] text-xl font-bold transition hover:border-white"
-            href="/about/team/">Our team</a>
+            href="/about-us/our-team/">Our team</a>
 
         <h1 class="wrap-words max-w-6xl text-6xl font-bold !tracking-normal lg:w-3/5 lg:text-8xl">
             {{ $user->name }}
